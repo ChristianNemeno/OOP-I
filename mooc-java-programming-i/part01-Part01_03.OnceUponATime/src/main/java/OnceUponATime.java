606f1b227fbd68fd2b1e3ef207784fdd2113ef09
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-08-21 16:56:51 +0800

    hey

--- mooc-java-programming-i/part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
+++
@@ -1,9 +0,0 @@
-
-public class OnceUponATime {
-
-    public static void main(String[] args) {
-        System.out.println("Once upon a time");// Write your program here
-        System.out.println("there was");// Write your program here
-        System.out.println("a program");// Write your program here
-    }
-}
