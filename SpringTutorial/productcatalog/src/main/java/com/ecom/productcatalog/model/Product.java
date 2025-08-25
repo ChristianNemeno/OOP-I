f1f1e8bdd8e28c71605b15bd060ae6d1bed3a19d
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-08-25 21:12:52 +0800

    The product model, using @ManyToOne , annotation to define a relation ship join column as well tells us what, where , column , deifnes Foreign key

--- SpringTutorial/productcatalog/src/main/java/com/ecom/productcatalog/model/Product.java
+++ SpringTutorial/productcatalog/src/main/java/com/ecom/productcatalog/model/Product.java
@@ -17,5 +17,7 @@ public class Product {
     @ManyToOne
     @JoinColumn(name="category_id", nullable=false)
     private Category category;
-
+    /**     The join column used for joining an entity association
+     *      means the category_id column in product will store the foreign key in Category entity
+     */
 }
