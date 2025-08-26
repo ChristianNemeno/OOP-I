f3071ce1eae564577b306cfbba9802aa1efd9fa2
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-08-26 19:47:19 +0800

    controller for product ,get all product using JPA repository (built in queries)

---
+++ SpringTutorial/productcatalog/src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.ecom.productcatalog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+
+}
