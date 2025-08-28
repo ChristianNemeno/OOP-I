d4c46fe17b9e4a8b742c376550d0e18a997c888f
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-08-28 22:54:24 +0800

    Student class

---
+++ Testing/src/Entity/Student.java
@@ -0,0 +1,15 @@
+package Entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private double allowance;
+    //private Wallet wallet;
+
+    public Student(int id, String name, double allowance) {
+        this.allowance = allowance;
+    }
+
+
+
+}
