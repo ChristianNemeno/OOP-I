2ef2379af48f1b9b075696262a864acf6cd5254f
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-08-29 08:41:55 +0800

    le wallet class and spend method

--- Testing/src/Entity/Student.java
+++ Testing/src/Entity/Student.java
@@ -5,11 +5,20 @@ import Util.Wallet;
 public class Student {
     private int id;
     private String name;
-    private double allowance;
+
     private Wallet wallet;
 
-    public Student(int id, String name, double allowance) {
-        this.allowance = allowance;
+    public Student(int id, String name) {
+        this.name = name;
+        this.id = id;
+        this.wallet = new Wallet(100);
+    }
+
+    public void spend(double amount) {
+        if(amount < 0){
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
+        this.wallet.deductMoney(amount);
     }
 
 
