97fce78de27296a267d727ad8fcc90a866298aac
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-09-04 16:10:36 +0800

    same

--- SpringTutorial/quiz-api/src/main/java/com/example/quiz_api/models/Quiz.java
+++ SpringTutorial/quiz-api/src/main/java/com/example/quiz_api/models/Quiz.java
@@ -2,6 +2,8 @@ package com.example.quiz_api.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +15,8 @@ public class Quiz {
     private Long id;
     private String title;
 
-    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<Question> questions;
 }
