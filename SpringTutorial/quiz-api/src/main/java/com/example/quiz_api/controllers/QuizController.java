ea2b3e9f6ad52799de243be1c960e51d50487794
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-09-05 17:32:38 +0800

    quiz controller

---
+++ SpringTutorial/quiz-api/src/main/java/com/example/quiz_api/controllers/QuizController.java
@@ -0,0 +1,43 @@
+package com.example.quiz_api.controllers;
+
+
+import com.example.quiz_api.models.Quiz;
+import com.example.quiz_api.services.QuizService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/quizzes")
+public class QuizController {
+
+    private QuizService quizService;
+
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+
+    @GetMapping
+    public List<Quiz> getAllQuizzes(){
+        return quizService.getAllQuizzes();
+    }
+    @GetMapping("/{id}")
+    public Quiz getQuizById(@PathVariable Long id){
+        return quizService.getQuizById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Quiz with id " + id + " not found")
+                ); // this if-else not found throws a http status 400 means not found
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Quiz addQuiz(@RequestBody Quiz quiz){
+        return quizService.saveQuiz(quiz);
+    }
+
+
+
+
+}
